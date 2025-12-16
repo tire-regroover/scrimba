250336e994d67b280544df0d58dfed8ddf7ba800
tire-regroover <nick.savolainen@gmail.com>
2025-12-15 22:40:21 -0500

    cool

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,9 @@ export default function Main() {
       <h1>Fun and Factual font kerning</h1>
       <ul>
         <li>This is where the primary content of the webpage goes</li>
-        <li>This is close enough</li>
+        <li>Put the <code>&lt;hr /&gt;</code> there on purpose</li>
+        <li>Didn't use no half image neither</li>
         <li>I don't care</li>
-        <li>I put the <code>&lt;hr /&gt;</code> there</li>
       </ul>
     </main>
   );
