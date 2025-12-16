54d3a5de6083b338fee0740cd5752fb2783f030a
tire-regroover <nick.savolainen@gmail.com>
2025-12-15 22:35:49 -0500

    say done

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <main className="main-content">
+      <h1>Fun and Factual font kerning</h1>
+      <ul>
+        <li>This is where the primary content of the webpage goes</li>
+        <li>This is close enough</li>
+        <li>I don't care</li>
+        <li>I put the <code>&lt;hr /&gt;</code> there</li>
+      </ul>
+    </main>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="navbar">
+          <img src="./logo192.png" className="nav-logo" alt="Logo" />
+          <span>MoldFacts</span>
+      </nav>
+    </header>
+  )
+}
